13b055898f2a2132e659ccb912ca638d7b12b423
Patrick Tolosa <patrick.tolosa@ladbrokescoral.com>
2018-11-25 18:43:06 +0530

    updated index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-/* entry point for production build */
+import Entity from 'lib/ECS/Entity';
+import entityLoop from 'lib/ECS/util/entityLoop';
+import ObjectPool from 'lib/ObjectPool/ObjectPool';
+import GameCanvas from 'lib/GameCanvas/GameCanvas';
+
+export {
+  Entity,
+  entityLoop,
+  ObjectPool,
+  GameCanvas
+};
